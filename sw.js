750cef7ec36591d5b30997e6631ed1f7690c6549
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2019-02-02 22:09:02 +0100

    back to simple sw

--- sw.js
+++ sw.js
@@ -1,3 +1,16 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
-workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
-workbox.routing.registerRoute(() => true, workbox.strategies.staleWhileRevalidate());
+self.addEventListener('fetch', function(event) {
+	console.log(event);
+  event.respondWith(
+    caches.open('files').then(function(cache) {
+			console.log('with cache');
+      return fetch(event.request).then(function(response) {
+				console.log('fetch succeeded', response);
+        cache.put(event.request, response.clone());
+        return response;
+			}, function(error) {
+				console.log('fetch failed', error);
+				return error;
+			});
+    })
+  );
+});

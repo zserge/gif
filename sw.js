042ff7146f6bf125894c820178a51f222820a3d5
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2019-02-02 21:57:21 +0100

    trying workbox

--- sw.js
+++ sw.js
@@ -1,18 +1,2 @@
-const cacheName = 'files';
-
-self.addEventListener('fetch', (event) => {
-  const request = event.request;
-  if (request.method !== 'GET') {
-    return;
-  }
-  event.respondWith(async function() {
-    const responseFromFetch = fetch(request);
-    event.waitUntil(async function() {
-      const responseCopy = (await responseFromFetch).clone();
-      const myCache = await caches.open(cacheName);
-      await myCache.put(request, responseCopy);
-    }());
-    const responseFromCache = await caches.match(request);
-    return responseFromCache || responseFromFetch;
-  }());
-});
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+workbox.routing.registerRoute(() => true, workbox.strategies.staleWhileRevalidate());

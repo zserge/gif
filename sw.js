9d1ad9bbd386201bee9d2a00feb13cb31fcc093d
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2019-02-02 22:03:24 +0100

    enable debug in sw

--- sw.js
+++ sw.js
@@ -1,2 +1,3 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
 workbox.routing.registerRoute(() => true, workbox.strategies.staleWhileRevalidate());

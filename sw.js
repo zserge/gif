0bf21f355a60eb8c724c38cd9495c40cdcabc67e
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2019-02-02 22:35:41 +0100

    back to original

--- sw.js
+++ sw.js
@@ -1,16 +1,26 @@
-self.addEventListener('fetch', function(event) {
-	console.log(event);
-  event.respondWith(
-    caches.open('files').then(function(cache) {
-			console.log('with cache');
-      return fetch(event.request).then(function(response) {
-				console.log('fetch succeeded', response);
-        cache.put(event.request, response.clone());
-        return response;
-			}, function(error) {
-				console.log('fetch failed', error);
-				return error;
-			});
-    })
-  );
+const cacheName = 'files';
+
+self.addEventListener('fetch', (event) => {
+  const request = event.request;
+  if (request.method !== 'GET') {
+    return;
+  }
+  event.respondWith(async function() {
+    const responseFromFetch = fetch(request);
+    event.waitUntil(async function() {
+      const responseCopy = (await responseFromFetch).clone();
+      const myCache = await caches.open(cacheName);
+      await myCache.put(request, responseCopy);
+    }());
+    if (request.headers.get('Accept').includes('text/html')) {
+      try {
+        return await responseFromFetch;
+      } catch(error) {
+        return caches.match(request);
+      }
+    } else {
+      const responseFromCache = await caches.match(request);
+      return responseFromCache || responseFromFetch;
+    }
+  }());
 });
